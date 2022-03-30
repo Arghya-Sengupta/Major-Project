5a0806013f4d0e931b5eacb1f94d03fcf89f379f
Arghya Sengupta <62299672+Arghya-Sengupta@users.noreply.github.com>
2022-03-30 22:53:29 +0530

    Add files via upload

---
+++ train_val_split.py
@@ -0,0 +1,42 @@
+# delete the UECFOOD100 folder first (if present)
+%rm -rf /content/UECFOOD100
+# code for spliting both images and labels for training and validation
+import os
+import glob
+import shutil
+from os.path import isfile, join
+from random import randint
+from os import listdir
+
+nc = 3	# number of catagories
+directory = '/content/sample_data/'
+
+def make_directories():
+	os.makedirs('/content/UECFOOD100/images/train')
+	os.mkdir('/content/UECFOOD100/images/val')
+	os.makedirs('/content/UECFOOD100/labels/train')
+	os.mkdir('/content/UECFOOD100/labels/val')
+
+def copy_files(probability):
+	for i in range(1,nc+1):
+		new_dir = directory + str(i) + '/'
+		for f in listdir(new_dir):
+			if isfile(join(new_dir, f)):
+				if f.endswith((".jpg", ".jpeg", ".png")):
+					file_name = os.path.splitext(f)[0]
+
+					split = 'train/'				
+					if(randint(1, 100) >= probability):	
+						split = 'val/'
+
+					imgage_src = new_dir + f
+					imgage_dst = "/content/UECFOOD100/images/" + split
+
+					label_src = directory + "labels/" + str(i) + "/" + file_name + ".txt"
+					label_dst = "/content/UECFOOD100/labels/" + split
+
+					shutil.copy2(imgage_src, imgage_dst)
+					shutil.copy2(label_src, label_dst)
+
+make_directories()
+copy_files(70)	# 70% for training and 30% for validation
