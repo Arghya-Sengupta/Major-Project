c2789ec9f13c9048c336ad928134b46110a5a676
Arghya Sengupta <62299672+Arghya-Sengupta@users.noreply.github.com>
2022-04-21 13:08:53 +0530

    Update detect_images.py

--- detect_images.py
+++ detect_images.py
@@ -1,19 +1,32 @@
-# from calculate_calories import *
-
-# total_calorie = get_foods()
-# print("Total Calories = ",total_calorie)
+# detect 75 images
 
 import os
+import time
 
-path = "D:/Project/"
-detection_path = path +  "Detection_Images/"
+path = "/content/drive/MyDrive/Data/" # D:/Project
+images_path = path +  "Detection_Images/"
 results_path = path + "Results/"
-N = len(os.listdir(detection_path))
-os.mkdir(results_path)
+N = len(os.listdir(images_path))
+start = time.time()
 
-for  i in range(1,N+1):
-	test_file = "test" + str(i) +  ".jpg"
-	command = "python /content/yolov5/detect.py --weights /content/yolov5/best.pt --conf 0.5 --source " + detection_path + test_file
-	status = os.system(command)
+if not os.path.exists(results_path):
+	os.mkdir(results_path)
+
+for test_file in os.listdir(images_path):
+	prog_path = "/content/yolov5/custom_detect.py"
+	weight_file = "/content/yolov5/runs/exp2/weights/best.pt"
+	test_file = images_path + test_file
+
+	command = "python " + prog_path + " --weights " + weight_file + " --conf 0.5 --source " + test_file
+	print("Detecting",test_file)
+
+	try:
+		status = os.system(command) # executing the command in cmd
+	except Exception as e:
+	    print("Error while executing custom_detect.py")
+	    print(e)
+
 	if(status != 0):
 		print(test_file,"not found")
+
+print(f"Detection Completed in {round(time.time()-start, 2)}s")
