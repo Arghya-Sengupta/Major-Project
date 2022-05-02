f7678492d95ee0c8091641b222296d8d6d8a448f
Arghya Sengupta <62299672+Arghya-Sengupta@users.noreply.github.com>
2022-05-02 19:20:33 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -7,29 +7,25 @@ from train_val_split import split_files
 from  detect_images import detect_file
 
 root = Tk()
-root.geometry("675x500+120+120")
-root.minsize(675,395)
-root.maxsize(675,395)
-# rootwalpaper=Image.open("rootwallpaper.jpg")
-# bgimg=ImageTk.PhotoImage(rootwalpaper)
-canvas=Canvas(root,width=675,height=500)
+#root.geometry("675x500+120+120")
+#root.minsize(675,395)
+#root.maxsize(675,395)
+canvas=Canvas(root,width=1000,height=1000)
 canvas.pack()
-# canvas.create_image(335,195,image=bgimg)
 root.title("CALORIE FINDER")
-# root.iconbitmap("icon.ico")
 
 def train():
     command = "python D:/Project/yolov5/train.py --batch 2 --epochs 100 --data D:/Project/yolov5/data/custom_coco.yaml --weights yolov5s.pt"
     try:
         status = os.system(command) # executing the command in cmd
     except Exception as e:
-        print("Error while executing train.py")
+        print("Error while Training")
         print(e)
     if(status != 0):
-        print("train.py not found")
+        print("Training stoped")
 
-def show_image():
-	im = Image.open(r"D:/Project/Result/result.jpg") 
+def show_result():
+	im = Image.open(r"D:/Project/Results/result.jpg") 
 	im.show()
 	pass
 
@@ -42,12 +38,8 @@ b2_placing=canvas.create_window(450,120,window=b2)
 b3=Button(root,text=" TRAIN ",font=("BankGothic Md BT",12,"bold"),fg="yellow",pady=20,padx=4,relief=RAISED,bg="black",command=lambda:[train()])
 b3_placing=canvas.create_window(452,250,window=b3)
 
-entry= Entry(root, text="Enter a file name from D:/Testing_Images", width=100)
-entry.focus_set()
-entry.pack()
-file_name = entry.get()
-
-b4=Button(root,text=" DETECT ",font=("BankGothic Md BT",12,"bold"),fg="yellow",padx=31,pady=3,relief=RAISED,bg="black",command=lambda:[detect_file(file_name), show_image()])
+file_name = "D:/Project/test.jpg"
+b4=Button(root,text=" DETECT ",font=("BankGothic Md BT",12,"bold"),fg="yellow",padx=31,pady=3,relief=RAISED,bg="black",command=lambda:[detect_file(file_name), show_result()])
 b4_placing=canvas.create_window(220,250,window=b4)
 
 if __name__ == '__main__':
