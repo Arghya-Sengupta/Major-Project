6cb4a9ef199947dbcbe5792ada0bfd71a6bf5c2d
Arghya Sengupta <62299672+Arghya-Sengupta@users.noreply.github.com>
2022-05-16 19:39:06 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -7,12 +7,9 @@ from train_val_split import split_files
 from  detect_images import detect_file
 
 root = Tk()
-#root.geometry("675x500+120+120")
-#root.minsize(675,395)
-#root.maxsize(675,395)
 canvas=Canvas(root,width=500,height=1000)
 canvas.pack()
-root.title("MAJOR PROJECT")
+root.title("Food Item Detection and Calorie Estimation using YOLOv5")
 
 def train():
     command = "python D:/Project/yolov5/train.py --batch 2 --epochs 100 --data D:/Project/yolov5/data/custom_coco.yaml --weights yolov5s.pt"
@@ -25,26 +22,22 @@ def train():
         print("Training stoped")
 
 def show_result():
-	im = Image.open(r"D:/Project/Results/result.jpg") 
-	im.show()
-	pass
+    im = Image.open(r"D:/Project/Results/result.jpg") 
+    im.show()
+    pass
 
-b1=Button(root,text="CREATE\nYOLO LABELS",font=("BankGothic Md BT",12,"bold"),fg="aquamarine",padx=25,pady=18,relief=RAISED,bg="black",command=lambda:[create_labels()])
+b1=Button(root,text=" CREATE\nYOLO LABELS ",font=("BankGothic Md BT",12,"bold"),fg="aquamarine",padx=25,pady=18,relief=RAISED,bg="black",command=lambda:[create_labels()])
 b1_placing=canvas.create_window(222,120,window=b1)
 
-b2=Button(root,text="CREATE\nTRAINING IMAGES",font=("BankGothic Md BT",12,"bold"),fg="aquamarine",padx=10,pady=18,relief=RAISED,bg="black",command=lambda:[split_files(70)])
+b2=Button(root,text=" CREATE\nTRAINING IMAGES ",font=("BankGothic Md BT",12,"bold"),fg="aquamarine",padx=10,pady=18,relief=RAISED,bg="black",command=lambda:[split_files(70)])
 b2_placing=canvas.create_window(222,240,window=b2)
 
-b3=Button(root,text="TRAIN",font=("BankGothic Md BT",12,"bold"),fg="red",padx=57,pady=28,relief=RAISED,bg="black",command=lambda:[train()])
+b3=Button(root,text=" TRAIN ",font=("BankGothic Md BT",12,"bold"),fg="yellow",padx=55,pady=28,relief=RAISED,bg="black",command=lambda:[train()])
 b3_placing=canvas.create_window(222,360,window=b3)
 
 file_name = "D:/Project/test.jpg"
-
-b4=Button(root,text="DETECT\nFROM IMAGE",font=("BankGothic Md BT",12,"bold"),fg="yellow",padx=30,pady=18,relief=RAISED,bg="black",command=lambda:[detect_file(file_name), show_result()])
+b4=Button(root,text=" DETECT ",font=("BankGothic Md BT",12,"bold"),fg="yellow",padx=47,pady=28,relief=RAISED,bg="black",command=lambda:[detect_file(file_name), show_result()])
 b4_placing=canvas.create_window(222,480,window=b4)
-
-#b5=Button(root,text="DETECT\nFROM CAMERA",font=("BankGothic Md #BT",12,"bold"),fg="yellow",padx=21,pady=18,relief=RAISED,bg="black",command=lambda:[detect_file(file_name), show_result()])
-#b5_placing=canvas.create_window(222,600,window=b5)
 
 if __name__ == '__main__':
     root.mainloop()
